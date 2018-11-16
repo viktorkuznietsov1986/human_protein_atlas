a5e0c002646ef9f527d56b86b7ec48e37d037f6c
viktorkuznietsov1986 <victor.kuzn@gmail.com>
2018-11-16 14:07:06 -0500

    quick update to inceptionv3 model

--- models/inceptionv3.py
+++ models/inceptionv3.py
@@ -6,7 +6,7 @@ from keras import applications
 
 def build_inceptionv3_classifier(input_shape, num_classes, use_dropout=False):
     inceptionv3 = applications.InceptionV3(include_top=False, input_shape=(input_shape[0], input_shape[1], 3))
-    inceptionv3.layers.pop(0)
+    #inceptionv3.layers.pop(0)
     inceptionv3.trainable = False
 
     inputs = Input(shape=(input_shape[0], input_shape[1], 4), name='in1')
