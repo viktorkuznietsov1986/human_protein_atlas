beeb24b580d90d9cd9fe5a8ea7f24a9ece899fc9
viktorkuznietsov1986 <victor.kuzn@gmail.com>
2018-11-21 12:08:52 -0500

    updated weighted_bce

--- loss.py
+++ loss.py
@@ -7,9 +7,9 @@ def focal_loss(y_true, y_pred, gamma=2, alpha=.25):
     return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.sum((1-alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
 
 
-def weighted_bce(args, outputs, targets):
+def weighted_bce(y_true, y_pred, pos_weight=20):
     '!!!try this out'
-    w = targets*args.pos_weight + 1  # default value of args.pos_weight is 20
+    w = y_true*pos_weight + 1
 
-    bce_loss = K.binary_cross_entropy_with_logits(outputs, targets, w)
+    bce_loss = K.binary_cross_entropy_with_logits(y_pred, y_true, w)
     return bce_loss
