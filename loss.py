694436d2854ffcbeedc3253d7cf80db7493a566a
viktorkuznietsov1986 <victor.kuzn@gmail.com>
2018-11-21 12:32:08 -0500

    udpated loss to include weights

--- loss.py
+++ loss.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import keras.backend as K
+from numpy import product
+
 
 def focal_loss(y_true, y_pred, gamma=2, alpha=.25):
     pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
@@ -7,9 +9,17 @@ def focal_loss(y_true, y_pred, gamma=2, alpha=.25):
     return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.sum((1-alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
 
 
-def weighted_bce(target, output, pos_weight=20):
+def weighted_bce(y_true, y_pred, pos_weight=20):
     '!!!try this out'
-    w = target*pos_weight + 1
+    weights = y_true*pos_weight + 1
 
-    bce_loss = K.mean(K.binary_crossentropy(target, output, w), axis=-1)
+    nb_cl = len(weights)
+    final_mask = K.zeros_like(y_pred[:, 0])
+    y_pred_max = K.max(y_pred, axis=1)
+    y_pred_max = K.reshape(y_pred_max, (K.shape(y_pred)[0], 1))
+    y_pred_max_mat = K.equal(y_pred, y_pred_max)
+    for c_p, c_t in product(range(nb_cl), range(nb_cl)):
+        final_mask += (weights[c_t, c_p] * y_pred_max_mat[:, c_p] * y_true[:, c_t])
+
+    bce_loss = K.mean(K.binary_crossentropy(y_true, y_pred)*final_mask, axis=-1)
     return bce_loss
