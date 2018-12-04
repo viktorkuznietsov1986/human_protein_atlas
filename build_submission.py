a1eec86dfc4a1273e5af4297263a43139753d7ea
viktorkuznietsov1986 <victor.kuzn@gmail.com>
2018-12-03 21:36:06 -0500

    updated the build submission script.

--- build_submission.py
+++ build_submission.py
@@ -12,6 +12,8 @@ input_shape = (300, 300)
 
 num_classes = 28
 
+n_channels = 3
+
 model = build_resnet50_classifier(input_shape, num_classes, l2_coeff=0.01)
 
 model.load_weights('model.h5')
@@ -20,6 +22,8 @@ predicted = []
 for name in submit['Id']:
     image = image_preprocess('../input/test/', name)
     image = cv2.resize(image, input_shape)
+    image = np.reshape(image, [1, input_shape[0], input_shape[1], n_channels])
+    prediction = model.predict(image)[0]
     score_predict = model.predict([image])
     label_predict = np.arange(num_classes)[score_predict>=0.5]
     str_predict_label = ' '.join(str(l) for l in label_predict)
