1db8c9d0fc2fadf0a2db6d68c723eb085a95c6ef
viktorkuznietsov1986 <victor.kuzn@gmail.com>
2018-12-05 10:37:55 -0500

    updated the build submission to use different thresholds and apply the xception model.

--- build_submission.py
+++ build_submission.py
@@ -2,8 +2,7 @@ import cv2
 import pandas as pd
 import numpy as np
 
-
-from models.resnet50 import build_resnet50_classifier
+from models.xception import build_xception_classifier
 from preprocess import image_preprocess
 
 submit = pd.read_csv('../input/sample_submission.csv')
@@ -14,22 +13,52 @@ num_classes = 28
 
 n_channels = 3
 
-model = build_resnet50_classifier(input_shape, num_classes, l2_coeff=0.01)
+model = build_xception_classifier(input_shape, num_classes, l2_coeff=0.01)
 
 model.load_weights('model.h5')
 
-predicted = []
-for name in submit['Id']:
-    image = image_preprocess('../input/test/', name)
-    image = cv2.resize(image, input_shape)
-    image = np.reshape(image, [1, input_shape[0], input_shape[1], n_channels])
-    prediction = model.predict(image)[0]
-    score_predict = model.predict([image])
-    label_predict = np.argwhere(score_predict>=0.5).flatten()
-    str_predict_label = ' '.join(str(l) for l in label_predict)
-    predicted.append(str_predict_label)
+def predict(model):
+    predicted02 = []
+    predicted03 = []
+    predicted04 = []
+    predicted05 = []
+
+    for name in submit['Id']:
+        image = image_preprocess('../input/test/', name)
+        image = cv2.resize(image, input_shape)
+        image = np.reshape(image, [1, input_shape[0], input_shape[1], n_channels])
+        prediction = model.predict(image)[0]
+
+        indices02 = np.argwhere(prediction >= 0.2).flatten()
+        labels02 = ' '.join(str(l) for l in indices02)
+
+        indices03 = np.argwhere(prediction >= 0.3).flatten()
+        labels03 = ' '.join(str(l) for l in indices03)
+
+        indices04 = np.argwhere(prediction >= 0.4).flatten()
+        labels04 = ' '.join(str(l) for l in indices04)
+
+        indices05 = np.argwhere(prediction >= 0.5).flatten()
+        labels05 = ' '.join(str(l) for l in indices05)
+
+        predicted02.append(labels02)
+        predicted03.append(labels03)
+        predicted04.append(labels04)
+        predicted05.append(labels05)
+
+        return predicted02, predicted03, predicted04, predicted05
 
 
-submit['Predicted'] = predicted
-submit.to_csv('submission.csv', index=False)
+predicted02, predicted03, predicted04, predicted05 = predict(model)
 
+submit['Predicted'] = predicted02
+submit.to_csv('submission02.csv', index=False)
+
+submit['Predicted'] = predicted03
+submit.to_csv('submission03.csv', index=False)
+
+submit['Predicted'] = predicted04
+submit.to_csv('submission04.csv', index=False)
+
+submit['Predicted'] = predicted05
+submit.to_csv('submission05.csv', index=False)
